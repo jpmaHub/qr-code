40ce4df45b324b7e20f9e24b1e6e6fe072be4f23
“Jaseera <j.p.mohamedabubacker@gmail.com>
2024-06-13 14:51:17 +0100

    Update styling

--- src/components/QRCodeCard.jsx
+++ src/components/QRCodeCard.jsx
@@ -2,13 +2,13 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import QRCode from '../assets/image-qr-code.png';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import QRCode from "../assets/image-qr-code.png";
 
 const theme = createTheme();
 
 theme.typography.h6 = {
-  color: 'hsl(218, 44%, 22%)',
+  color: "hsl(218, 44%, 22%)",
   fontOpticalSizing: "auto",
   fontWeight: 700,
   fontStyle: "normal",
@@ -16,8 +16,8 @@ theme.typography.h6 = {
 };
 
 theme.typography.body1 = {
-  paddingTop: '10px',
-  color:"hsl(220, 15%, 55%)",
+  paddingTop: "10px",
+  color: "hsl(220, 15%, 55%)",
   fontOpticalSizing: "auto",
   fontWeight: 400,
   fontStyle: "normal",
@@ -27,28 +27,31 @@ theme.typography.body1 = {
 export default function QRCodeCard() {
   return (
     <ThemeProvider theme={theme}>
-    <Card sx={{ width: "320px", height: "497px",borderRadius: 3, boxShadow: 3 }}>
-      <CardMedia
-        sx={{ height: "288px",  width: "288px", borderRadius: 4.5, m: 2 }}
-        image={QRCode}
-        title="qr code"
-      />
-      <CardContent sx={{ textAlign: "center" }}>
-        <Typography
-          gutterBottom
-          component="div"
-          variant="h6"
+      <Card
+        sx={{ width: "320px", height: "497px", borderRadius: 4, boxShadow: 3 }}
+      >
+        <CardMedia
+          sx={{ height: "288px", width: "288px", borderRadius: 2, m: 2 }}
+          image={QRCode}
+          title="qr code"
+        />
+        <CardContent
+          sx={{
+            textAlign: "center",
+            paddingTop: "14px",
+            paddingLeft: "24px",
+            paddingRight: "20px;",
+          }}
         >
-          Improve your front-end skills by building projects
-        </Typography>
-        <Typography
-          variant="body1"
-        >
-          Scan the QR code to visit Frontend Mentor and take your coding skills
-          to the next level
-        </Typography>
-      </CardContent>
-    </Card>
+          <Typography gutterBottom component="div" variant="h6">
+            Improve your front-end skills by building projects
+          </Typography>
+          <Typography variant="body1">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </Typography>
+        </CardContent>
+      </Card>
     </ThemeProvider>
   );
 }
